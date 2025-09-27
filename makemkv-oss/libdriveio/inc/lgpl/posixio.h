25ccdf3f6a917f6f7291ffef4585750ee646bb41
FabioLolix <fabio.loli@disroot.org>
2025-09-27 23:37:00 +0200

    1.17.7

--- makemkv-oss/libdriveio/inc/lgpl/posixio.h
+++ makemkv-oss/libdriveio/inc/lgpl/posixio.h
@@ -1,7 +1,7 @@
 /*
     libDriveIo - MMC drive interrogation library
 
-    Copyright (C) 2007-2023 GuinpinSoft inc <libdriveio@makemkv.com>
+    Copyright (C) 2007-2024 GuinpinSoft inc <libdriveio@makemkv.com>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -83,6 +83,32 @@ int     puts_utf8(char const* _Buffer);
 int     fputs_utf8(char const* _Buffer, FILE* _Stream);
 int     fprintf_utf8(FILE* const _Stream, char const* const _Format, ...);
 
+typedef struct _dirent_DIR DIR;
+
+struct dirent
+{
+    long		    d_ino;		/* Always zero. */
+    long            d_off;      /* Always zero. */
+    unsigned short	d_reclen;	/* Always zero. */
+    unsigned char   d_type;	    /* File type. */
+    unsigned short	d_namlen;	/* Length of name in d_name. */
+    char		    d_name[FILENAME_MAX]; /* File name. */
+};
+
+#define DT_UNKNOWN	0
+#define DT_FIFO	    1
+#define DT_CHR		2
+#define DT_DIR		4
+#define DT_BLK		6
+#define DT_REG		8
+#define DT_LNK		10
+#define DT_SOCK	    12
+#define DT_WHT		14
+
+DIR*            opendir_utf8(const char * _Path);
+struct dirent*  readdir(DIR* dir);
+int             closedir(DIR* dir);
+
 
 #ifdef __cplusplus
 };
@@ -92,6 +118,7 @@ int     fprintf_utf8(FILE* const _Stream, char const* const _Format, ...);
 
 #include <sys/param.h>
 #include <sys/mount.h>
+#include <dirent.h>
 
 #ifndef _DARWIN_FEATURE_64_BIT_INODE
 #error "_DARWIN_FEATURE_64_BIT_INODE not defined"
@@ -116,12 +143,16 @@ int     fprintf_utf8(FILE* const _Stream, char const* const _Format, ...);
 #define rmdir_utf8  rmdir
 #define mkdir_utf8  mkdir
 #define rename_utf8 rename
+#define opendir_utf8    opendir
 
 #define puts_utf8       puts
 #define fputs_utf8      fputs
 #define fprintf_utf8    fprintf
 
 #elif defined(_linux_)
+
+#include <dirent.h>
+
 
 #define O_BINARY 0
 
@@ -133,6 +164,7 @@ int     fprintf_utf8(FILE* const _Stream, char const* const _Format, ...);
 #define rmdir_utf8  rmdir
 #define mkdir_utf8  mkdir
 #define rename_utf8 rename
+#define opendir_utf8    opendir
 
 #define puts_utf8       puts
 #define fputs_utf8      fputs
